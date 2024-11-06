80436460e7703958b42caed3ed47a4b888be1f29
Gugustinette <mercier.augustin@outlook.fr>
2024-11-06 22:17:55 +0100

    fix: upgrade.ts

--- src/commands/upgrade.ts
+++ src/commands/upgrade.ts
@@ -2,13 +2,13 @@ import { ConsoleUtil } from "../util/ConsoleUtil";
 import { ProcessUtil } from "../util/ProcessUtil";
 
 /**
- * The upgrade command is responsible for upgrading the current Fibbo project
+ * The upgrade command is responsible for upgrading the current Fibbo project to the latest version
  */
 export const upgrade = () => {
   ConsoleUtil.log("⏳ Upgrading project...");
 
   /**
-   * Read the package.json to find every `@fibbojs` dependency
+   * Read the package.json to find every `@fibbojs` dependency and update them to the latest version
    */
   ProcessUtil.run("cat package.json")
     .then((packageJsonRaw) => {
@@ -20,13 +20,11 @@ export const upgrade = () => {
         ConsoleUtil.warn("No `@fibbojs` dependencies found in the project, it seems you are running this command in a non-Fibbo project");
         return;
       } else {
-        /**
-         * Upgrade every `@fibbojs` dependency
-         */
+        // Update every `@fibbojs` dependency to the latest version
         const upgradeCommand = `npm install ${fibboDependencies.map((dependency) => `${dependency}@latest`).join(" ")}`;
         ProcessUtil.run(upgradeCommand)
           .then(() => {
-            ConsoleUtil.log("✅ Project upgraded");
+            ConsoleUtil.log("✅ Project upgraded successfully");
           })
           .catch((error) => {
             ConsoleUtil.throw("Error while upgrading the project : " + error);
